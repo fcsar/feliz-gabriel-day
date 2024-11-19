9e25c4537d33fe4066cc5be6de86cd5a17bbe89e
Francisco Cesar <francisco.mateus@lit.com.br>
2024-11-19 13:04:48 -0300

    feat: add new project

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelector('.gift').addEventListener('click', function () {
+    const message = document.getElementById('surprise-message');
+    message.classList.remove('hidden');
+  });
+  
